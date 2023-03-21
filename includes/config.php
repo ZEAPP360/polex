e362cd4e2a27aced62b600407fc94b64c341f579
imujeebco <imad.m.mujeeb@gmail.com>
2023-03-21 09:34:45 +0500

    Initial Commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php  
+    define("DB_NAME", 'db_abdeldb');
+    define("DB_USER", 'root');
+    define("DB_PASSWORD", '');
+    define("DB_HOST", 'localhost');
+    define("DB_PORT", '3306');
+    define("SYS_URL", '');
+    define("DEBUGGING", true);
+    define("DEFAULT_LOCALE", 'en_us');
+    define("LICENCE_KEY", '6ABPQlZeV-3J6mu-488oB-3vFl7-3a5K8-3b0227261dba');
+?>
